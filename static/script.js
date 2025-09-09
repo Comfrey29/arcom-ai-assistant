476e28ab231434760829030741a5d22cfee42f54
Comfrey29 <tiopepa37@gmail.com>
2025-09-09 03:38:54 -1200

    script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,11 @@
+async function askQuestion() {
+    const question = document.getElementById("question").value;
+    const response = await fetch("/ask", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text: question })
+    });
+    const data = await response.json();
+    document.getElementById("answer").innerText = data.answer;
+}
 
