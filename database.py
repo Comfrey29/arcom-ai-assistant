e891b0df56dd0e2fd0b8b0d229a79c99149aae0c
Comfrey29 <tiopepa37@gmail.com>
2025-10-05 09:59:04 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    username = db.Column(db.String(80), primary_key=True, nullable=False)
+    password = db.Column(db.LargeBinary, nullable=False)
+    is_premium = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+class PremiumKey(db.Model):
+    __tablename__ = 'premium_keys'
+    key = db.Column(db.String(64), primary_key=True, nullable=False)
+    used = db.Column(db.Boolean, default=False)
+    uses_left = db.Column(db.Integer, default=10)
+    expires_at = db.Column(db.DateTime, nullable=True)
